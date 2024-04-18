fa672636a5c2a86a3655e63d6f69e63f4879f9b9
Daniel Parsons <dpars@hotmail.co.uk>
2024-04-19 00:43:00 +0900

    key issue fix

--- src/components/InfoSwiper.tsx
+++ src/components/InfoSwiper.tsx
@@ -39,8 +39,8 @@ const InfoSwiper: React.FC<IInfoSwiperProps> = (props) => {
       <div className="grid grid-cols-2 w-full h-[623px] bg-white rounded-2xl">
         <div className="bg-secondary p-[70px] flex flex-col gap-6 text-white font-bold rounded-l-2xl relative">
           <div className="text-3xl">{props.items[activeSlide].subTitle}</div>
-          { props.items[activeSlide].description.map((text) => {
-            return <div className="text-base">{text}</div>
+          { props.items[activeSlide].description.map((text, index) => {
+            return <div key={index} className="text-base">{text}</div>
           }) }
           <ul className="splide__pagination info-swiper-pagination"></ul>
         </div>
